bb8c5b619b4dd6376b255f46603908b320edef0e
Sesha Kumar <sesha911@gmail.com>
2016-01-26 09:21:21 -0800

    Sesha: Added data extraction scripts. Song attributes saved for reference.

---
+++ extract_data_scripts/user_list_song_list_creator.py
@@ -0,0 +1,50 @@
+import sys
+filed = open(sys.argv[1], "r")
+write_users = open("user_list.csv", "w+")
+write_songs = open("song_list.csv", "w+")
+write_counts = open("user_song_count.csv", "w+")
+
+users = {}
+songs = {}
+counter = 0
+user_counter = 0
+song_counter = 0
+user_list = ""
+song_list = ""
+counts_list = ""
+
+for line in filed:
+	user, song, count = line.split("\t")
+	if user not in users:
+		user_counter+=1
+		users[user] = user_counter
+		user_list += str(users[user]) +","+ user+"\n"
+
+	if song not in songs:
+		song_counter+=1
+		songs[song] = song_counter
+		song_list += str(songs[song]) +","+ song+"\n"
+
+	counts_list += str(users[user]) +","+ str(songs[song]) +","+ count
+	counter += 1
+
+	if counter > 10000:
+		write_users.write(user_list)
+		write_songs.write(song_list)
+		write_counts.write(counts_list)
+
+		counter = 0
+		user_list = ""
+		song_list = ""
+		counts_list = ""
+
+if counter > 0:
+	write_users.write(user_list)
+	write_songs.write(song_list)
+	write_counts.write(counts_list)
+
+
+write_counts.close()		
+write_songs.close()		
+write_users.close()
+filed.close()	
