e055de5aac8ba3e1b8dc049da27c0ce86c3693f0
Sesha Kumar <sesha911@gmail.com>
2016-01-26 19:31:48 -0800

    Sesha: Secured configuration information

--- spark_scripts/batch/query_cassandra_spark.py
+++
@@ -1,9 +0,0 @@
-from pyspark import SparkConf
-import pyspark_cassandra
-from pyspark_cassandra import CassandraSparkContext
-
-conf = SparkConf().setAppName("PySpark Cassandra").setMaster("spark://ip-172-31-2-132:7077").set("spark.cassandra.connection.host", "52.89.0.21")
-sc = CassandraSparkContext(conf=conf)
-usr = sc.cassandraTable("usersong", "usrsng")
-rows = usr.select("user_id", "req_time", "song_id").where("user_id=?", 1)
-
