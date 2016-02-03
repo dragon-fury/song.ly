f8890204203cdf4bb7a67cb48e9cbde5b70e33e6
Sesha Kumar <sesha911@gmail.com>
2016-02-03 15:15:13 -0800

    Sesha: Updated cassandra schema file to match the current changes in schema

--- cassandra_scripts/cassandra_schema.py
+++ cassandra_scripts/cassandra_schema.py
@@ -1,7 +1,7 @@
 from cassandra.cluster import Cluster
 
 # Connect to cassandra cluster
-cluster = Cluster(['seed_node'])
+cluster = Cluster([CASSANDRA_SEED_NODE_IP])
 session = cluster.connect()
 
 # Create and set cassandra keyspace to work
@@ -9,10 +9,11 @@ session.execute("CREATE KEYSPACE usersong WITH replication = {'class':'SimpleStr
 session.set_keyspace("usersong")
 
 # Create tables to insert data
-session.execute("CREATE TABLE usrsngcnt (time int, user_id int, song_id int, count int, primary key (time, user_id, song_id));")
+session.execute("CREATE TABLE usrsnglog (time int, user_id int, song_id int, count int, primary key (time, user_id, song_id));")
+session.execute("CREATE TABLE usrconnection (user_id int, follows_id int, relevance_score float, primary key(user_id, follows_id, relevance_score));")
 session.execute("CREATE TABLE usrsng (user_id int, req_time int, song_id int,  primary key (user_id, req_time)) WITH CLUSTERING ORDER BY (req_time DESC);")
 session.execute("CREATE TABLE sngusr (song_id int, req_time int, user_id int,  primary key (song_id, req_time)) WITH CLUSTERING ORDER BY (req_time DESC);")
-session.execute("CREATE TABLE usrusr (user_id int, accept_time int, follow_user_id int,  primary key (user_id, accept_time)) WITH CLUSTERING ORDER BY (accept_time DESC);")
+session.execute("CREATE TABLE usrrelevance (user_id int, time int, suggest_user_id int, relevance_score int, primary key (user_id, time)) WITH CLUSTERING ORDER BY (time DESC);")
 
 # Close the connection
 session.shutdown()
