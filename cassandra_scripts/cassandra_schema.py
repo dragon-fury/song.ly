513ddf63781f8ee19f97ac1033d183e7d4f5ffae
Sesha Kumar <sesha911@gmail.com>
2016-02-11 12:34:50 -0800

    Sesha: Added missing declaration for user_song_log table

--- cassandra_scripts/cassandra_schema.py
+++ cassandra_scripts/cassandra_schema.py
@@ -9,9 +9,10 @@ session.execute("CREATE KEYSPACE usersong WITH replication = {'class':'SimpleStr
 session.set_keyspace("usersong")
 
 # Create tables to insert data
-session.execute("CREATE TABLE user_connections (user_id int, follows_id int, relevance_score float, primary key(user_id, follows_id));")
+session.execute("CREATE TABLE user_song_log (timestamp int, user_id int, song_id int, primary key(user_id, song_id));")
 session.execute("CREATE TABLE user_to_song (user_id int, req_time int, song_id int,  primary key (user_id, req_time)) WITH CLUSTERING ORDER BY (req_time DESC);")
 session.execute("CREATE TABLE song_to_user (song_id int, req_time int, user_id int,  primary key (song_id, req_time)) WITH CLUSTERING ORDER BY (req_time DESC);")
+session.execute("CREATE TABLE user_connections (user_id int, follows_id int, relevance_score float, primary key(user_id, follows_id));")
 session.execute("CREATE TABLE user_relevance (user_id int, timestamp int, suggested_user_id int, relevance_score float, primary key (user_id, timestamp, suggested_user_id)) WITH CLUSTERING ORDER BY (time DESC);")
 
 # Close the connection
