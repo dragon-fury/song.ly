2026b73fe45097412c6882a6ef041f83a7edcbb0
Sesha Kumar <sesha911@gmail.com>
2016-02-11 12:43:58 -0800

    Sesha: Updated cassandra schema with configuration variables

--- cassandra_scripts/cassandra_schema.py
+++ cassandra_scripts/cassandra_schema.py
@@ -1,19 +1,20 @@
+import config
 from cassandra.cluster import Cluster
 
 # Connect to cassandra cluster
-cluster = Cluster([CASSANDRA_SEED_NODE_IP])
+cluster = Cluster([config.CASSANDRA_SEED_NODE_IP])
 session = cluster.connect()
 
 # Create and set cassandra keyspace to work
-session.execute("CREATE KEYSPACE usersong WITH replication = {'class':'SimpleStrategy', 'replication_factor':3};")
-session.set_keyspace("usersong")
+session.execute("CREATE KEYSPACE"+ config.CASSANDRA_KEYSPACE +"WITH replication = {'class':'SimpleStrategy', 'replication_factor':3};")
+session.set_keyspace(config.CASSANDRA_KEYSPACE)
 
 # Create tables to insert data
-session.execute("CREATE TABLE user_song_log (timestamp int, user_id int, song_id int, primary key(user_id, song_id));")
+session.execute("CREATE TABLE user_song_log (timestamp int, user_id int, song_id int, primary key(timestamp, user_id));")
 session.execute("CREATE TABLE user_to_song (user_id int, req_time int, song_id int,  primary key (user_id, req_time)) WITH CLUSTERING ORDER BY (req_time DESC);")
-session.execute("CREATE TABLE song_to_user (song_id int, req_time int, user_id int,  primary key (song_id, req_time)) WITH CLUSTERING ORDER BY (req_time DESC);")
+session.execute("CREATE TABLE song_to_user (song_id int, req_time int, user_id int,  primary key (song_id, req_time, user_id)) WITH CLUSTERING ORDER BY (req_time DESC);")
 session.execute("CREATE TABLE user_connections (user_id int, follows_id int, relevance_score float, primary key(user_id, follows_id));")
-session.execute("CREATE TABLE user_relevance (user_id int, timestamp int, suggested_user_id int, relevance_score float, primary key (user_id, timestamp, suggested_user_id)) WITH CLUSTERING ORDER BY (time DESC);")
+session.execute("CREATE TABLE user_relevance (user_id int, timestamp int, suggested_user_id int, relevance_score float, primary key (user_id, timestamp, suggested_user_id)) WITH CLUSTERING ORDER BY (timestamp DESC);")
 
 # Close the connection
 session.shutdown()
